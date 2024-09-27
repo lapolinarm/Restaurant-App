153148745e1aa00a925e51ddc8a0714e2f35ef93
Levi Apolinar <leviam51@gmail.com>
2024-09-26 21:52:44 -0500

    cards text is left aligned

--- src/app/components/RestaurantCard.js
+++ src/app/components/RestaurantCard.js
@@ -1,14 +1,13 @@
 // src/app/components/RestaurantCard.js
-
 import React from 'react';
 
 export default function RestaurantCard({ restaurant }) {
   return (
-    <div className="border rounded-lg shadow-lg p-4 flex flex-col items-center text-center">
+    <div className="border rounded-lg shadow-lg p-4 flex flex-col text-left">
       <h2 className="text-xl font-semibold">{restaurant.name}</h2>
       <p>{restaurant.address}</p>
       <p className="text-gray-500">{restaurant.category}</p>
-      <div className="mt-2">
+      <div className="flex justify-center mt-4 space-x-2">
         <button className="bg-blue-500 text-white text-sm px-2 py-1 rounded mr-2">Ver detalles</button>
         <button className="bg-red-500 text-white text-sm px-2 py-1 rounded">Borrar</button>
       </div>
