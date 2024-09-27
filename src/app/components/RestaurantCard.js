7fb6eb751c356021b2504ad992cb534948d601f4
Levi Apolinar <leviam51@gmail.com>
2024-09-26 23:30:35 -0500

    styles were applied to the cards

--- src/app/components/RestaurantCard.js
+++ src/app/components/RestaurantCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default function RestaurantCard({ restaurant }) {
   return (
-    <div className="border rounded-lg shadow-lg p-4 flex flex-col text-left transition-shadow duration-300 hover:shadow-2xl max-w-80 h-44">
+    <div className="border rounded-lg shadow-lg p-4 flex flex-col text-left transition-shadow duration-300 hover:shadow-2xl min-w-[250px] min-h-[180px]">
       <h2 className="text-xl font-semibold">{restaurant.name}</h2>
       <p>{restaurant.address}</p>
-      <p className="text-gray-500">{restaurant.category}</p>
-      <div className="flex justify-center mt-4 space-x-2">
+      <p className="text-gray-500 flex-grow">{restaurant.category}</p>
+      <div className="flex justify-center mt-auto space-x-2">
         <button className="bg-blue-500 text-white text-sm px-2 py-1 rounded mr-2">Ver detalles</button>
         <button className="bg-red-500 text-white text-sm px-2 py-1 rounded">Borrar</button>
       </div>
