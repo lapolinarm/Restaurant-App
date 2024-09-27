ec75d6d19173840d9f1e73309df5c21ed8067881
Levi Apolinar <leviam51@gmail.com>
2024-09-27 10:33:27 -0500

    added unsplash domain in next.config.mjs file for proper image processing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['unsplash.com'], // Agregamos el dominio de Unsplash
+  },
+};
 
 export default nextConfig;
