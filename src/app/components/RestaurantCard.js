16d91761c5acab3f4834109b578383dc0175db62
Levi Apolinar <leviam51@gmail.com>
2024-10-02 21:43:17 -0500

    The background colors were changed, including the cards and buttons

--- src/app/components/RestaurantCard.js
+++ src/app/components/RestaurantCard.js
@@ -33,20 +33,26 @@ export default function RestaurantCard({ restaurant, onDelete }) {
   };
 
   return (
-    <div className="border rounded-lg shadow-lg p-4 flex flex-col text-left transition-shadow duration-300 hover:shadow-2xl min-w-[250px] min-h-[180px]">
+    <div className="bg-[#2c2c2c] border rounded-lg shadow-lg p-4 flex flex-col text-left min-w-[250px] min-h-[180px]
+    [background:linear-gradient(45deg,
+    #172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient
+    (from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme
+    (colors.purple.600)_86%,_theme(colors.purple.500)_90%,_theme(colors.
+    purple.600)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl
+    border border-transparent animate-border">
       <h2 className="text-xl font-semibold">{restaurant.name}</h2>
       <p>{restaurant.address}</p>
       <p className="text-gray-500 flex-grow">{restaurant.category}</p>
       <div className="flex justify-center mt-auto space-x-0">
         <button
-          className="bg-blue-500 text-white text-sm px-2 py-1 rounded mr-2"
+          className="bg-[#13CE83] text-white text-sm px-2 py-1 rounded mr-2"
           onClick={handleDetailsClick}
         >
           Ver detalles
         </button>
 
         <button
-          className="border-2 border-red-500 text-red-500 text-sm px-2 py-1 rounded bg-transparent"
+          className="border-2 border-white-500 text-white-500 text-sm px-2 py-1 rounded bg-transparent"
           onClick={handleDeleteClick} // Cambia a handleDeleteClick
         >
           Borrar
